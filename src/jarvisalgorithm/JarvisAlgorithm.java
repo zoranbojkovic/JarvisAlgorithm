458b228adc32f5b60b6c8590b72b5c80ff3ed1a2
zoranbojkovic <78708702+zoranbojkovic@users.noreply.github.com>
2021-06-16 12:34:36 +0200

    Final commit
    ispis, pozivanje metode u main metodi

--- src/jarvisalgorithm/JarvisAlgorithm.java
+++ src/jarvisalgorithm/JarvisAlgorithm.java
@@ -5,28 +5,40 @@ import java.util.Scanner;
 
 public class JarvisAlgorithm {
 
-    public boolean method(double a, double b, double c, double d, double e, double f){
+    public static boolean method(double a, double b, double c, double d, double e, double f){
       return ((d-b)*(e-c) - (c-a)*(f-d)) >=0;  
     }
     
-    public void JarvisAlgorithm(double[]x, double[]y){
+    public static void JarvisAlgorithm(double[]x, double[]y){
         int l = 0;
         int[]array = new int[8];
-        for (int i = 1; i < 8; i++) {
+        for (int i = 0; i < 8; i++) {
+            array[i] = -1;
             if(x[i] < x[l]){
                 l = i;
             }
         }
         int a = l, b = (a+1)%8;
+        a = b;
         while(a != l){
-            b = (a+1)%8;
+            
             for (int i = 0; i < 8; i++) {
                 if(method(x[a],y[a],x[i],y[i],x[b],y[b])){
                     b = i;
                 }
             }
             array[a] = b;
+            b = (a+1)%8;
             a = b;
+        }
+        print(x, y, array);
+    }
+    
+    public static void print(double[]x, double[]y, int[]array){
+        for (int i = 0; i < 8; i++) {
+            if(array[i] != -1){
+                System.out.println("(" + x[i] + " , " + y[i] + ")");
+            }
         }
     }
     
@@ -39,6 +51,8 @@ public class JarvisAlgorithm {
             x[i] = sc.nextDouble();
             y[i] = sc.nextDouble();
         }
+        System.out.println("");
+        JarvisAlgorithm(x, y);
                 
     }
     
