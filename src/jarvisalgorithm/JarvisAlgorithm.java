b598787d38b4d532824c7d2ac3ccbc0cf78b0800
zoranbojkovic <78708702+zoranbojkovic@users.noreply.github.com>
2021-06-16 11:55:04 +0200

    Unos podataka
    Unošenje 8  x i y koordinata preko Scanner-a

--- src/jarvisalgorithm/JarvisAlgorithm.java
+++ src/jarvisalgorithm/JarvisAlgorithm.java
@@ -8,6 +8,14 @@ public class JarvisAlgorithm {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double[]x = new double[8];
+        double[]y = new double[8];
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Unesite x, pa y koordinatu " + (i+1) + ". tačke");
+            x[i] = sc.nextDouble();
+            y[i] = sc.nextDouble();
+        }
+                
     }
     
 }
