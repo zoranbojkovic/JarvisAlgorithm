9202ad5a4dedf1e6f45ab9c5d3df51712566b1a2
zoranbojkovic <78708702+zoranbojkovic@users.noreply.github.com>
2021-06-16 11:51:14 +0200

    Scanner
    Unošenje scanner-a, brisanje nepotrebnih linija koda

--- src/jarvisalgorithm/JarvisAlgorithm.java
+++ src/jarvisalgorithm/JarvisAlgorithm.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jarvisalgorithm;
 
-/**
- *
- * @author zokii
- */
+import java.util.Scanner;
+
+
 public class JarvisAlgorithm {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
     }
     
 }
